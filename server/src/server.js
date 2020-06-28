34279751ae29099da24381f5b10e156185191ac9
jakelizzI <jakelizzI@gmail.com>
2020-06-28 22:09:38 +0900

    UUIDを指定して振り分けることができるようにしてみた

--- server/src/server.js
+++ server/src/server.js
@@ -4,17 +4,29 @@ const port = 5001;
 
 const server = new Server({port:port});
 
-server.on('connection', (ws) => {
-  
-  ws.on('message', (mes) => {
+const clientMap = new Map();
 
-    server.clients.forEach( (client) => {
-      client.send(mes);
-    });
+server.on('connection', (ws, req) => {
+
+  const uuid = req.headers['sec-websocket-protocol'];
+  console.log(`uuid : ${uuid}`);
+  if (uuid) {
+    clientMap.set(uuid, ws);
+  }
+  
+  ws.on('message', (data) => {
+    const dataObj = JSON.parse(data);
+    console.log(`uuid from browser : ${dataObj.uuid}`);
+    clientMap.get(dataObj.uuid).send(dataObj.message);
   });
 
-  ws.on('close', () => {
-    console.log('I lost a client');
+  ws.on('close', function() {
+    clientMap.forEach((value, key) => {
+      if (value === this) {
+        console.log('I lost a client : ' + key);
+        clientMap.delete(key);
+      }
+    });
   });
 });
 
--- view/src/ComeSouView.tsx
+++ view/src/ComeSouView.tsx
@@ -1,43 +1,48 @@
 import React from "react";
-import { Container, Input, Form, Header, Icon } from "semantic-ui-react";
+import { Container, Input, Form, Header, Icon, Menu } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
-
-interface State {
-  comment: string;
-}
+import SettingModal from "./SettingModal";
+import connect from './connection';
+import { State } from './interfaces';
 
 class ComeSouView extends React.Component<{}, State> {
   private sock: WebSocket;
 
   constructor(props: Readonly<{}>) {
     super(props);
-    this.state = { comment: "" };
-    this.handleChange = this.handleChange.bind(this);
+    this.state = { uuid: "", comment: "" };
+    this.handleCommentChange = this.handleCommentChange.bind(this);
+    this.handleUUIDChange = this.handleUUIDChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
 
-    this.sock = new WebSocket("ws://comesou-server:5001");
+    this.sock = connect();
     this.sock.addEventListener("open", () => {
       console.log("接続成功");
     });
   }
+  handleUUIDChange(event: React.ChangeEvent<HTMLInputElement>) {
+    this.setState({ uuid: event.target.value });
+  }
 
-  handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+  handleCommentChange(event: React.ChangeEvent<HTMLInputElement>) {
     this.setState({ comment: event.target.value });
   }
 
   handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    const { comment } = this.state;
-    console.log(`submit : ${comment}`);
-    this.sock.send(comment);
+    const { uuid, comment } = this.state;
+    this.sock.send(`{"uuid" : "${uuid}", "message": "${comment}"}`);
     this.setState({ comment: "" });
   }
 
   render() {
-    const { comment } = this.state;
+    const { uuid, comment } = this.state;
     return (
       <Container textAlign="center">
+        <Menu secondary>
+          <SettingModal />
+        </Menu>
         <Header as="h2" icon textAlign="center">
           <Icon name="comments outline" />
           <Header.Content>コメ送</Header.Content>
@@ -45,13 +50,18 @@ class ComeSouView extends React.Component<{}, State> {
         <Form onSubmit={this.handleSubmit}>
           <Form.Field>
             <Input
+              placeholder="UUID"
+              onChange={this.handleUUIDChange}
+              value={uuid}
+            />
+            <Input
               action={{
                 color: "blue",
                 content: "送信",
                 type: "submit",
               }}
               placeholder="コメント"
-              onChange={this.handleChange}
+              onChange={this.handleCommentChange}
               value={comment}
             />
           </Form.Field>
